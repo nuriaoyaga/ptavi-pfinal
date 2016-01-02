b44f4c73b52df9435c67961fea1d7c567d57292f
Nuria O <nuria.oyaga@gmail.com>
2016-01-02 13:17:37 +0100

    Lectura del fichero config en cliente

--- uaclient.py
+++ uaclient.py
@@ -8,15 +8,56 @@ import socket
 import sys
 
 # Cliente UDP simple.
-if len(sys.argv) != 3:
-    sys.exit('Usage: python client.py method receiver@IP:SIPport')
+if len(sys.argv) != 4:
+    sys.exit('Usage: python uaclient.py config method option')
 try:
-    METOD = sys.argv[1].upper()
-    RECEPTOR = sys.argv[2].split('@')[0]
-    IP_REC = sys.argv[2].split('@')[1].split(':')[0]
-    PORT_REC = int(sys.argv[2].split('@')[1].split(':')[1])
+    CONFIG = sys.argv[1]
+    METOD = sys.argv[2].upper()
+    OPTION = sys.argv[3]
 except Exception:
-    sys.exit('Usage: python client.py method receiver@IP:SIPport')
+    sys.exit('Usage: python uaclient.py config method option')
+METODOS = ['INVITE', 'BYE', 'REGISTER']
+if METOD not in METODOS:
+    sys.exit('Usage: python uaclient.py config method option')
+
+#Apertura del fichero de configuración
+fich = open(CONFIG, 'r')
+line = fich.readlines()
+fich.close()
+
+#USUARIO
+line_account = line[1].split(">")
+account = line_account[0].split("=")[1]
+USUARIO = account.split(" ")[0][1:-1]
+#CONTRASEÑA
+passw = line_account[0].split("=")[2]
+PASSWORD = passw.split(" ")[0][1:-1]
+#IP
+line_uaserver = line[2].split(">")
+uaserver = line_uaserver[0].split("=")[1]
+IP = uaserver.split(" ")[0][1:-1]
+#PUERTO
+uaserver1 = line_uaserver[0].split("=")[2]
+PUERTO = uaserver1.split(" ")[0][1:-1]
+#PUERTO AUDIO RTP
+line_rtp = line[3].split(">")
+rtpaudio = line_rtp[0].split("=")[1]
+PUERTO_AUDIO = rtpaudio.split(" ")[0][1:-1]
+#IP DEL PROXY
+line_regproxy = line[4].split(">")
+regproxy = line_regproxy[0].split("=")[1]
+IP_PROXY = regproxy.split(" ")[0][1:-1]
+#PUERTO DEL PROXY
+regproxy1 = line_regproxy[0].split("=")[2]
+PUERTO_PROXY = regproxy1.split(" ")[0][1:-1]
+#PATH DEL LOG
+line_log = line[5].split(">")
+log = line_log[0].split("=")[1]
+PATH_LOG = log.split(" ")[0][1:-1]
+#PATH DEL AUDIO
+line_audio = line[6].split(">")
+audio = line_audio[0].split("=")[1]
+PATH_AUDIO = audio.split(" ")[0][1:-1]
 
 # Contenido que vamos a enviar
 LINE = METOD + ' sip:' + RECEPTOR + '@' + IP_REC + ' SIP/2.0\r\n'
